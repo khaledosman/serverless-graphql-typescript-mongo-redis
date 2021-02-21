de58fa7f6d17b972c30044646f4941ca873adca0
khaledosman <khaled.osmaan@gmail.com>
2021-02-21 11:24:04 +0100

    update deps

--- api/sendPushNotificationHandler.ts
+++ api/sendPushNotificationHandler.ts
@@ -1,7 +1,7 @@
 import 'source-map-support/register'
 import { setupXRay } from '../helpers/setup-xray'
 import { SQSHandler, SQSEvent, Context } from 'aws-lambda'
-import { sendFcmMessage, getAccessToken } from '../helpers/firebase-cloud-messaging'
+// import { sendFcmMessage, getAccessToken } from '../helpers/firebase-cloud-messaging'
 
 setupXRay()
 
@@ -9,13 +9,13 @@ export const handler: SQSHandler = async function (event: SQSEvent, context: Con
   context.callbackWaitsForEmptyEventLoop = false
   await Promise.all(event.Records.map(async record => {
     console.log(record)
-    const { body } = record
+    // const { body } = record
 
-    const { pushToken, title, message } = JSON.parse(body)
-    const token = await getAccessToken()
-    await sendFcmMessage(pushToken, token, {
-      title: title,
-      body: message
-    })
+    // const { pushToken, title, message } = JSON.parse(body)
+    // const token = await getAccessToken()
+    // await sendFcmMessage(pushToken, token, {
+    //   title: title,
+    //   body: message
+    // })
   }))
 }
